4b0e720266fdaa08a26268cec0d08117fbe0ca7c
YashKapoor1004 <yashkapoor1004@gmail.com>
2022-07-23 23:53:45 -0400

    Longest Common Prefix

---
+++ 14.py
@@ -0,0 +1,7 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        for i in range(len(min(strs, key=len))):
+            for x in range(len(strs)-1):
+              if strs[x][:i+1] != strs[x+1][:i+1]:
+                return strs[x][:i]
+        return (min(strs))
