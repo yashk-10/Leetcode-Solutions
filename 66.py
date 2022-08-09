2206b48cee90436379ba1830522bd811ccb26987
YashKapoor1004 <yashkapoor1004@gmail.com>
2022-08-08 23:44:50 -0400

    test

--- 66.py
+++ 66.py
@@ -2,9 +2,9 @@ class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         if digits[-1] != 9:
             digits[-1] += 1
-        elif digits[-1] == 9 and len(digits) == 1:
-            digits[0] = 1
-            digits.append(0)
+        # elif digits[-1] == 9 and len(digits) == 1:
+        #     digits[0] = 1
+        #     digits.append(0)
         else:
             digits[-1] = 0
             digits[-2] += 1
