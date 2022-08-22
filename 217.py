4bbd59aca51aaa6a3b1104d4c05e25fa14ab7064
YashKapoor1004 <yashkapoor1004@gmail.com>
2022-08-21 23:19:58 -0400

    Contains Duplicate

--- 217.py
+++ 217.py
@@ -1,3 +1,9 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        return len(nums) != len(set(nums))
+        hashMap = {}
+        for i in range(len(nums)):
+            if nums[i] not in hashMap:
+                hashMap[nums[i]] = True
+            else:
+                return hashMap[nums[i]]
+        return False
